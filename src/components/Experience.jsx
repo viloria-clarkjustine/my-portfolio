9b470f8242f22f3bf0005167e756ffd105865e9c
viloria-clarkjustine <viloria.clarkj@gmail.com>
2026-09-14 11:55:53 +0800

    initially created the structure of the Navbar

--- src/components/Experience.jsx
+++
@@ -1,7 +0,0 @@
-export default function Experience() {
-  return (
-    <section id="experience" className="debug">
-      <h1>EXPERIENCE</h1>
-    </section>
-  );
-}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,114 +1,47 @@
-export default function Navbar() {
-  const handleNavigation = (e) => {
-    setTimeout(() => {
-      window.history.replaceState(null, null, window.location.pathname);
-    }, 0);
-  };
+import logo from "../assets/pattern_square.png";
 
+export default function Navbar() {
   return (
-    <nav className="sticky top-0 debug flex justify-center items-center py-2">
-      <div className="flex justify-center items-center rounded-full p-1.5 gap-4 bg-card/20">
-        <a
-          href="#home"
-          className="flex justify-center items-center rounded-full text-black p-2 bg-brand bg-card/90"
-          onClick={handleNavigation}
-          aria-label="Home"
-        >
-          <svg
-            className="w-8 h-8"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 320"
+    <nav className="flex items-center justify-center border border-red-500 p-2">
+      <ul className="flex items-center gap-4 border border-red-500 p-2 rounded-full">
+        <li>
+          <a
+            href="#home"
+            aria-label="Home"
+            className="flex items-center justify-center w-10 h-10 border border-red-500 rounded-full"
           >
-            <rect x="0" y="64" width="64" height="65" fill="currentColor" />
-            <rect x="0" y="128" width="64" height="65" fill="currentColor" />
-            <rect x="0" y="192" width="64" height="65" fill="currentColor" />
-            <rect x="64" y="0" width="64" height="65" fill="currentColor" />
-            <rect x="64" y="256" width="64" height="65" fill="currentColor" />
-            <rect x="128" y="64" width="64" height="65" fill="currentColor" />
-            <rect x="128" y="192" width="64" height="65" fill="currentColor" />
-            <rect x="192" y="256" width="64" height="65" fill="currentColor" />
-            <rect x="256" y="0" width="64" height="65" fill="currentColor" />
-            <rect x="256" y="64" width="64" height="65" fill="currentColor" />
-            <rect x="256" y="128" width="64" height="65" fill="currentColor" />
-            <rect x="256" y="192" width="64" height="65" fill="currentColor" />
-            <rect x="320" y="256" width="64" height="65" fill="currentColor" />
-            <rect x="384" y="192" width="64" height="65" fill="currentColor" />
-            <rect x="448" y="0" width="64" height="65" fill="currentColor" />
-            <rect x="448" y="64" width="64" height="65" fill="currentColor" />
-            <rect x="448" y="128" width="64" height="65" fill="currentColor" />
-          </svg>
-        </a>
-        <a
-          href="#experience"
-          className="flex items-center p-3 rounded-full hover:bg-card/50 hover:text-black transition font-bold gap-2"
-          onClick={handleNavigation}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-briefcase-business-icon lucide-briefcase-business"
+            <img
+              src={logo}
+              alt="Site Logo"
+              className="w-6 h-6 object-contain"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="#skills"
+            className="inline-flex items-center justify-center border border-red-500 px-4 py-2 rounded-full"
           >
-            <path d="M12 12h.01" />
-            <path d="M16 6V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v2" />
-            <path d="M22 13a18.15 18.15 0 0 1-20 0" />
-            <rect width="20" height="14" x="2" y="6" rx="2" />
-          </svg>
-          Experience
-        </a>
-        <a
-          href="#projects"
-          className="flex items-center p-3 rounded-full hover:bg-card/50 hover:text-black transition font-bold gap-2"
-          onClick={handleNavigation}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-folder-kanban-icon lucide-folder-kanban"
+            Skills
+          </a>
+        </li>
+        <li>
+          <a
+            href="#experience"
+            className="inline-flex items-center justify-center border border-red-500 px-4 py-2 rounded-full"
           >
-            <path d="M4 20h16a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.93a2 2 0 0 1-1.66-.9l-.82-1.2A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13c0 1.1.9 2 2 2Z" />
-            <path d="M8 10v4" />
-            <path d="M12 10v2" />
-            <path d="M16 10v6" />
-          </svg>
-          Projects
-        </a>
-        <a
-          href=""
-          className="flex items-center p-3 rounded-full bg-card/90 text-black font-bold gap-2"
-          onClick={handleNavigation}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-mail-icon lucide-mail"
+            Experience
+          </a>
+        </li>
+        <li>
+          <a
+            href="#socials"
+            className="inline-flex items-center justify-center border border-red-500 px-4 py-2 rounded-full"
           >
-            <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
-            <rect x="2" y="4" width="20" height="16" rx="2" />
-          </svg>
-          viloria.clarkj@gmail.com
-        </a>
-      </div>
+            Socials
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
